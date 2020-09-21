aa89e8a24d8941b8fd2ed5ac01c2e43dd20a76fc
Giovanna Gennari Jungklaus <g198010@dac.unicamp.br>
2020-09-21 15:06:12 -0300

    Anotações 21/set - introdução a C

--- Anotações de aula/Unidade 1/maximo.c
+++ Anotações de aula/Unidade 1/maximo.c
@@ -1,9 +1,3 @@
-"""
-Encontrar o máximo entre a e b 
-
-Lembrar: toda instrução precisa de um ;
-"""
-
 #include <stdio.h>
 
 int maximo(int a, int b) {
@@ -15,9 +9,8 @@ int maximo(int a, int b) {
 }
 
 int potencia(int a, int b) {
-    int i;
-    int prod 1 ;
-    for(i=0; i<b; i++){
+    int i, prod = 1;
+    for(i = 0; i < b; i++) {
         prod = a * prod;
     }
     return prod;
@@ -26,10 +19,10 @@ int potencia(int a, int b) {
 int main() {
     int a, b, maior, pot;
     printf("Entre com a e b\n");  
-    scanf(" %d %d", &a, &b); 
+    scanf("%d %d", &a, &b); 
     maior = maximo(a, b);
-    pot = potencia(a,b);
+    pot = potencia(a, b);
     printf("Maior: %d\n", maior);
-    printf("a^b: %d\n", maior);
+    printf("a^b: %d\n", pot);
     return 0;
 }
