79c9ae7c223bd622890884c4ab0713dd205e8087
Giovanna_Gennari <g198010@dac.unicamp.br>
2020-09-23 14:37:09 -0300

    descreve problema megasena

---
+++ tarefa01/megasena.c
@@ -0,0 +1,27 @@
+/*
+Mega Sena
+
+Da dvisão do prêmio:
+6 acertos - 62% 
+5 acertos - 19%
+4 acertos - 19%
+
+Os ganhadores da mesma faixa dividem o prêmio entre si 
+
+Entradas:
+1ª linha - int m (nº de apostadores) e double n (valor do prêmio)
+m matriz binárias de dimesão 6 x 10
+1 = nº marcado; 0 = não marcado
+última linha - 6 números sorteados
+
+Saída:
+m linhas onde a i-ésima linha é o valor do prêmio a ser recebido pelo i-ésimo apostador
+
+Restrições:
+
+nº apostas --> 5 <= m <= 1000
+valor do prêmio --> 1e6 <= n <= 1e9 -> ou seja, basta ser int (32 bits)
+
+
+
+*/
