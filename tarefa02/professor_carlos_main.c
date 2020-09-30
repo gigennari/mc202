1a3164c96914f924ed53a73328335c5aa386f6ac
Giovanna_Gennari <g198010@dac.unicamp.br>
2020-09-30 15:32:58 -0300

    parte operacoes

--- tarefa02/professor_carlos_main.c
+++ tarefa02/professor_carlos_main.c
@@ -5,10 +5,11 @@ Cliente
 #include <stdio.h>
 #include <professor_carlos.h>
 #define N 50 //numero máx de turmas
-#define M 40 //numero max de alunos por turma 
+#define M 40 //numero max de alunos por turma
 #define K 50 //numero max de operacoes
 
-Aluno ler_aluno(){
+Aluno ler_aluno()
+{
     Aluno A;
     int dia, mes, ano;
     ("%c %c %d %d %d", &A.nome, &A.sobrenome, &dia, &mes, &ano);
@@ -18,31 +19,68 @@ Aluno ler_aluno(){
     return A;
 }
 
-Turma criar_turmas(Turma turmas[], int num_turmas){
-    for(int i = 0; i < num_turmas; i++)
-    {
-        int alunos;
-        scanf("%d ", &alunos);
-        for(int j = 0; j < alunos; j++){
+Turma criar_turmas(Turma turmas[], int num_turmas)
+{
+    for (int i = 0; i < num_turmas; i++)
+    {    
+        int qtd_alunos = 0;
+        scanf("%d ", &qtd_alunos);
+        for (int j = 0; j < qtd_alunos; j++)
+        {
             Aluno A;
             A = ler_aluno();
-            add_aluno(turmas[], A, i);
+            add_aluno(turmas, A, i);
         }
     }
 }
 
-void realizar_operacao(int num_operacoes){
-
-
+int realizar_operacoes(Turma turmas[], int qtd_turmas, int num_operacoes)
+{
+    for (int i = 0; i < num_operacoes; i++)
+    {
+        int operacao;
+        scanf("%d ", &operacao);
+        if (operacao == 1)
+        {
+            int j;
+            scanf("%d ", &j);
+            procura_novo_na_turma(turmas, qtd_turmas, j);
+        }
+        else if (operacao == 2)
+        {
+            int j;
+            scanf("%d ", &j);
+            procura_velho_na_turma(turmas, qtd_turmas, j);
+        }
+        else if (operacao == 3)
+        {
+            /* code */
+        }
+        else if (operacao == 4)
+        {
+            /* code */
+        }
+        else if (operacao == 5)
+        {
+            /* code */
+        }
+        else if (operacao == 6)
+        {
+            /* code */
+        }
+        else
+        {
+            printf("Essa operacao nao eh valida.\n")
+        }
+        
+    }
 }
 
-
-int main(){
-    int num_turmas, num_operacoes;
+int main()
+{
+    int qtd_turmas, num_operacoes;
     Turma turmas[N];
-    scanf("%d %d", &num_turmas, num_operacoes);
-    criar_turmas(turmas, num_turmas);
-    
-
-
+    scanf("%d %d", &qtd_turmas, num_operacoes);
+    criar_turmas(turmas, qtd_turmas);
+    realizar_operacoes(turmas, qtd_turmas, num_operacoes);
 }
