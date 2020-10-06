323675e824fa820f9c2eacafa0861a69351e1f2d
Giovanna_Gennari <g198010@dac.unicamp.br>
2020-10-06 15:11:28 -0300

    cria stubs das funções

--- tarefa03/fakenews.c
+++ tarefa03/fakenews.c
@@ -21,31 +21,76 @@ typedef struct Termos{
     double desvio_padrao;
 }Termo;
 
-typedef struct Cada_Categoria{
+typedef struct Categoria{
+    char nome[20]; 
     int n;
     char *termos;
 }C;
 
-typedef struct Todas_Categorias{
-    C Bot;
-    C Surpreendente;
-    C Normal;
-    C Local;
-    C Irrelevante;
+Termo le_termos_teclado(int num_termos, int dias, Termo *termos){
 
-}Categorias;
+}
+
+void encontra_maximo(){
+
+}
+
+void encontra_minimo(){
+
+}
+
+void calcula_media(){
+
+}
+
+
+void calcula_desvio_padrao(){
+
+}
+
+/**
+ * Calcula maximo, mínimo, média e desvio para cada termo
+ * */
+void calcular_dados(int num_termos, int dias, Termo *termos){
+
+}
+
+void imprime_dados(Termo *termos, int num_termos){
+    
+}
+
+C inicializa_categorias(C categorias){
+
+}
+
+C aloca_termos_categorias(C todas, Termo *termos, int num_termos){
+
+}
+
+void imprime_categorias(){
+
+}
 
 
 
 int main(){
     int num_termos, dias;
     Termo *termos;
+    C *categorias;
+
     scanf("%d %d", &num_termos, &dias);
     termos = malloc(num_termos * sizeof(Termo));
+    le_termos_teclado(num_termos, dias, termos);
+
+    calcular_dados(num_termos, dias, termos);
+    imprime_dados(termos, num_termos);
+
+    inicializa_categorias(*categorias);
+    aloca_termos_categorias(*categorias, termos, num_termos);
+    imprime_categorias(categorias);
 
 
-
+    free(termos->relevancia_diaria);
     free(termos);
     return 0;
-    
 }
