81ccdae4173c19305294c696efa095cac01c42eb
Giovanna_Gennari <g198010@dac.unicamp.br>
2020-10-15 09:43:16 -0300

    cria potencia recursiva

--- Exercícios de Fixação/Unidade 7/potencia_recur.c
+++ Exercícios de Fixação/Unidade 7/potencia_recur.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int potencia_recursiva(int base, int expoente){
+    if (expoente == 0)
+        return 1;
+    else
+        return base * potencia(base, expoente-1);
     
 
 }
