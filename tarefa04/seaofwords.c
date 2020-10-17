b72387e85f5cde88fd251519a8b1e5c519ca259e
Giovanna_Gennari <g198010@dac.unicamp.br>
2020-10-16 21:52:47 -0300

    procura recursivamente alterada/ não le palavras

--- tarefa04/seaofwords.c
+++ tarefa04/seaofwords.c
@@ -36,12 +36,9 @@ void libera_matriz(char **matriz, int n, int m)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
-        {
-            free(matriz[i][j]);
-        }
         free(matriz[i]);
     }
+    free(matriz);
 }
 
 void ler_matriz(char **matriz, int n, int m)
@@ -55,61 +52,101 @@ void ler_matriz(char **matriz, int n, int m)
     }
 }
 
+int fora_do_limite(int n, int m, int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 int procura_recur(char *palavra, char **texto, int n, int m, int chamada, int i, int j)
 {
 
-    if (chamada = 0)
+    /**
+     * Retornamos 1 (verdadeiro) quando chegamos ao final da palavra e nenhuma chamada falhou)
+     * */
+    if (palavra[chamada] == '\0')
     {
-        for (int linha = i; linha < n; linha++)
+        return 1;
+    }
+    /**
+     * Retornamos 0 (falso) quando está fora do limite da matriz, se a posição não é a proxima letra ou se já visitamos a posição
+     * */
+
+    else if (fora_do_limite(n, m, i, j) || texto[i][j] != palavra[chamada] || texto[i][j] == '-')
+    {
+        return 0;
+    }
+    else if (texto[i][j] == palavra[chamada])
+    {
+        char aux = texto[i][j];
+        texto[i][j] = '-';
+
+        if (procura_recur(palavra, texto, n, m, chamada + 1, i + 1, j))
         {
-            for (int coluna = j; coluna < m; coluna++)
+            texto[i][j] = aux;
+            return 1;
+        }
+        else if (procura_recur(palavra, texto, n, m, chamada + 1, i - 1, j))
+        {
+            texto[i][j] = aux;
+            return 1;
+        }
+
+        else if (procura_recur(palavra, texto, n, m, chamada + 1, i, j + 1))
+        {
+            texto[i][j] = aux;
+            return 1;
+        }
+        else if (procura_recur(palavra, texto, n, m, chamada + 1, i, j - 1))
+        {
+            texto[i][j] = aux;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int encontra_primeira_letra(char *palavra, char **texto, int n, int m, int chamada, int i, int j)
+{
+
+    for (int linha = 0; linha < n; linha++)
+    {
+        for (int coluna = 0; coluna < m; coluna++)
+        {
+            if (texto[linha][coluna] == palavra[chamada])
             {
-                if (texto[i][j] = palavra[chamada])
+                char aux = texto[linha][coluna];
+                texto[linha][coluna] = '-';
+                i = linha;
+                j = coluna;
+                if (procura_recur(palavra, texto, n, m, chamada + 1, i, j))
                 {
-                    i = linha;
-                    j = coluna;
-                    return procura_recur(palavra, texto, n, m, chamada + 1, i, j);
+                    return 1;
                 }
                 else
                 {
                     return 0;
                 }
-
-
-
+                texto[linha][coluna] = aux;
             }
         }
     }
-    if (chamada > 0){
-        if(texto[i+1][j] = palavra[chamada]){
-            i += 1;
-            return procura_recur(palavra, texto, n, m, chamada + 1, i, j);
-        }
-        else if(texto[i-1][j] = palavra[chamada]){
-            i -= 1;
-            return procura_recur(palavra, texto, n, m, chamada + 1, i, j);            
-        }
-        else if(texto[i][j+1] = palavra[chamada]){
-            j += 1;
-            return procura_recur(palavra, texto, n, m, chamada + 1, i, j);
-        }
-        else if(texto[i][j-1] = palavra[chamada]){
-            j -= 1;
-            return procura_recur(palavra, texto, n, m, chamada + 1, i, j);
-        }
-        else{
-            return 0;
-        }
-        
-    }
+    return 0;
 }
 
 void caca_palavras(char **palavras, char **texto, int qtd, int n, int m)
 {
-    for (int q = 0; q < qtd; q++)
+    int q;
+    for (q = 0; q < qtd; q++)
     {
         int chamada = 0, i = 0, j = 0;
-        if (procura_recur(palavras[q][TAM], texto, n, m, chamada, i, j))
+        if (encontra_primeira_letra(palavras[q], texto, n, m, chamada, i, j))
         {
             printf("sim");
         }
@@ -128,12 +165,19 @@ int main()
     /**
      * Aloca memoria dinamicamente
      * */
-    char **texto, *palavras_buscadas;
+    char **texto, **palavras_buscadas;
     texto = aloca_matriz(n, m);
     palavras_buscadas = aloca_matriz(q, TAM);
 
     ler_matriz(texto, n, m);
     ler_matriz(palavras_buscadas, q, TAM);
+
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            printf("%c", palavras_buscadas[i][j]);
+        }
+        printf("\n");
+        }
 
     caca_palavras(palavras_buscadas, texto, q, n, m);
 
