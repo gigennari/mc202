8307e6d067c6ecebbc2020ff6a0fb12814fd3715
Giovanna_Gennari <g198010@dac.unicamp.br>
2020-11-09 15:01:01 -0300

    começa multiplicação

--- tarefa05/calculadora.c
+++ tarefa05/calculadora.c
@@ -28,7 +28,6 @@ p_lista criar_lista()
     return NULL;
 }
 
-
 p_lista add_direita(p_lista lista, int c)
 {
 
@@ -87,17 +86,20 @@ void imprime_lista(p_lista lista)
     {
         p_no atual;
         atual = lista->ini;
-        
-        do{
+
+        do
+        {
             printf("%d", atual->dado);
             atual = atual->prox;
-        }while(atual != NULL);
-        
+        } while (atual != NULL);
+        printf('\n');
+
         free(atual);
     }
 }
 
-void liberar_memoria(p_lista lista){
+void liberar_memoria(p_lista lista)
+{
     p_no no;
     no = lista->ini;
     while (no != NULL)
@@ -108,30 +110,31 @@ void liberar_memoria(p_lista lista){
     }
     free(lista);
     free(no);
-    
 }
-
 
 /**
  * Implementacao das operacoes da calculadora
  * */
 
-
 //soma
-void somar_em_no(p_lista num1, p_lista num2, p_lista resultado, int x){
-    
-    if ((num1->ini == num1->fim)  && (num2->ini == num2->fim)){
+void somar_em_no(p_lista num1, p_lista num2, p_lista resultado, int x)
+{
+
+    if ((num1->ini == num1->fim) && (num2->ini == num2->fim))
+    {
         resultado = add_esquerda(resultado, x);
     }
-    else if (num1->ini == num1->fim){
-        num2->fim->ant->dado += 1; 
+    else if (num1->ini == num1->fim)
+    {
+        num2->fim->ant->dado += 1;
     }
     else if (num2->ini == num2->fim)
     {
         num1->fim->ant->dado += 1;
     }
 }
-void add_resultado(p_lista num1, p_lista num2, p_lista resultado, int soma){
+void add_resultado(p_lista num1, p_lista num2, p_lista resultado, int soma)
+{
     if (soma > 10)
     {
         int x = soma % 10;
@@ -150,7 +153,7 @@ p_lista soma(p_lista resultado, p_lista num1, p_lista num2)
     for (int i = 0; (i < num1->tamanho) && (i < num2->tamanho); i++)
     {
         soma = num1->fim->dado + num2->fim->dado;
-        
+
         add_resultado(num1, num2, resultado, soma);
 
         num1->fim = num1->fim->ant;
@@ -163,15 +166,17 @@ p_lista soma(p_lista resultado, p_lista num1, p_lista num2)
      * */
     if (num1->ini != num1->fim)
     {
-        while(num1->ini != num1->fim){
+        while (num1->ini != num1->fim)
+        {
             resultado = add_esquerda(resultado, num1->fim->dado);
             num1->fim = num1->fim->ant;
         }
     }
     else if (num2->ini != num2->fim)
     {
-        
-        while(num2->ini != num2->fim){
+
+        while (num2->ini != num2->fim)
+        {
             resultado = add_esquerda(resultado, num2->fim->dado);
             num2->fim = num2->fim->ant;
         }
@@ -180,42 +185,50 @@ p_lista soma(p_lista resultado, p_lista num1, p_lista num2)
     return resultado;
 }
 
-
-
-
 //subtrai
 
-p_lista completar_zeros(p_lista num, int qtde){
-    for (int i = 0; i < qtde; i++){
+p_lista completar_zeros(p_lista num, int qtde)
+{
+    for (int i = 0; i < qtde; i++)
+    {
         num = add_esquerda(num, 0);
     }
+    return num;
 }
 
-p_lista numero_maior(p_lista num1, p_lista num2){
+p_lista numero_maior(p_lista num1, p_lista num2)
+{
 
-    if (num1->tamanho > num2->tamanho){
+    if (num1->tamanho > num2->tamanho)
+    {
         num2 = completar_zeros(num2, num1->tamanho - num2->tamanho);
         return num1;
     }
-    else if (num2->tamanho > num1->tamanho){
+    else if (num2->tamanho > num1->tamanho)
+    {
         num1 = completar_zeros(num1, num2->tamanho - num1->tamanho);
         return num2;
     }
-    else{
+    else
+    {
         p_no atual_1, atual_2;
         atual_1 = num1->ini;
         atual_2 = num2->ini;
 
-        do{
-            if(atual_1->dado > atual_2->dado){
+        do
+        {
+            if (atual_1->dado > atual_2->dado)
+            {
                 return num1;
             }
-            else {
-                return num2;
+            else if (atual_2->dado > atual_1->dado)
+            {
+                    return num2;
             }
+            
             atual_1 = atual_1->prox;
             atual_2 = atual_2->prox;
-        }while (atual_1 != num1->fim);
+        } while (atual_1 != num1->fim);
 
         //se forem iguais, tanto faz qual devolver
         return num1;
@@ -226,40 +239,62 @@ p_lista subtrai(p_lista resultado, p_lista num1, p_lista num2)
 {
     p_lista maior, menor;
     maior = numero_maior(num1, num2);
-    if (maior == num1){
+    if (maior == num1)
+    {
         menor = num2;
     }
-    else{
+    else
+    {
         menor = num1;
     }
-    
+
     int diferenca;
-    do{
-        if (maior->fim->dado < menor->fim->dado){
+    do
+    {
+        if (maior->fim->dado < menor->fim->dado)
+        {
             maior->fim->ant->dado -= 1;
             maior->fim->dado += 10;
-        }        
+        }
         diferenca = maior->fim->dado - menor->fim->dado;
         resultado = add_esquerda(resultado, diferenca);
         maior->fim = maior->fim->ant;
         menor->fim = menor->fim->ant;
-    }while(maior->fim != maior->ini);
+    } while (maior->fim != maior->ini);
 
     return resultado;
 }
 
+//multiplica
 /**
- * 
 p_lista multiplica(p_lista resultado, p_lista num1, p_lista num2)
 {
-    return NULL; 
-} 
+    p_lista parcial;
+    p_no atual;
+    atual = num2->fim;
+    int iter = 0;
+    int produto, restante;
+
+    do
+    {
+        do
+        {
+            produto = num1->fim->dado * atual->dado;
+
+
+            iter += 1;
+            atual = atual->ant;
+        } while (atual != num2->ini);
+
+        num1->fim = num1->fim->ant;
+
+    } while (num1->fim != num1->ini);
+}
 */
 
 /**
  * Implementacao das funcoes principais
  * */
-
 
 void executa_operacao(char operacao, p_lista num1, p_lista num2)
 {
@@ -269,16 +304,17 @@ void executa_operacao(char operacao, p_lista num1, p_lista num2)
     {
         resultado = soma(resultado, num1, num2);
     }
-    
+
     else if (operacao == '-')
     {
         resultado = subtrai(resultado, num1, num2);
     }
-    /**
+     /**
     else if (operacao == '*')
     {
         resultado = multiplica(resultado, num1, num2);
     }
+   
     else if ((operacao == '/'))
     {
         printf("%d", 0);
@@ -286,23 +322,25 @@ void executa_operacao(char operacao, p_lista num1, p_lista num2)
     */
 
     imprime_lista(resultado);
-    printf("\n");
 
     liberar_memoria(num1);
     liberar_memoria(num2);
     liberar_memoria(resultado);
 }
 
-p_lista ler_numero(p_lista num){
+p_lista ler_numero(p_lista num)
+{
     char caractere;
     scanf("%c", &caractere);
-    while(caractere <= '9' && caractere >= '0'){
-    
-            if (caractere != '\0'){
-                int algarismo = caractere - '0';
-                num = add_direita(num, algarismo);
-                scanf("%c", &caractere);
-            }       
+    while (caractere <= '9' && caractere >= '0')
+    {
+
+        if (caractere != '\0')
+        {
+            int algarismo = caractere - '0';
+            num = add_direita(num, algarismo);
+            scanf("%c", &caractere);
+        }
     }
 
     return num;
@@ -327,4 +365,3 @@ int main()
     }
     return 0;
 }
-
