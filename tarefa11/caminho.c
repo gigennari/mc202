e5ddc06dc55304180052921b1627a555e9ae121f
Giovanna_Gennari <g198010@dac.unicamp.br>
2020-12-27 09:07:23 -0300

    cria structs pontos e grafo

--- tarefa11/caminho.c
+++ tarefa11/caminho.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum
+{
+    pokestop,
+    lugia
+} Categoria;
+
 typedef struct Ponto{
     int x, y;
     int aresta;
+    Categoria categoria;
+    struct Ponto *prox;
 } Ponto;
 
+typedef Ponto *p_ponto;
 
+typedef struct Grafo{
+    p_ponto *adj;
+    int n;
+} Grafo;
+
+typedef Grafo *p_grafo;
 
 int main(){
+    p_ponto inicio = malloc(sizeof(Ponto));
+    scanf("%d, %d", inicio->x, inicio->y);
 
 
     return 0;
