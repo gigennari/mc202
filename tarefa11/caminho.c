68f04bbd2d69101ff6466a9e0c733dc634a860da
Giovanna_Gennari <g198010@dac.unicamp.br>
2021-01-04 22:06:06 -0300

    correcoes

--- tarefa11/caminho.c
+++ tarefa11/caminho.c
@@ -11,7 +11,7 @@ typedef struct Ponto
 //grafo usando matriz
 typedef struct Grafo
 {
-    int **adj; //armazena distâncias entre os pontos[i][j]
+    double **adj; //armazena distâncias entre os pontos[i][j]
     int n;
 } Grafo;
 
@@ -22,10 +22,10 @@ p_grafo criar_grafo(int n)
     int i, j;
     p_grafo g = malloc(sizeof(Grafo));
     g->n = n;
-    g->adj = malloc(n * sizeof(int *));
+    g->adj = malloc(n * sizeof(double *));
     for (i = 0; i < n; i++)
     {
-        g->adj[i] = malloc(n * sizeof(int));
+        g->adj[i] = malloc(n * sizeof(double));
     }
 
     for (i = 0; i < n; i++)
@@ -38,7 +38,8 @@ p_grafo criar_grafo(int n)
     return g;
 }
 
-void libera_grafo(p_grafo g){
+void libera_grafo(p_grafo g)
+{
     for (int i = 0; i < g->n; i++)
     {
         free(g->adj[i]);
@@ -47,9 +48,17 @@ void libera_grafo(p_grafo g){
     free(g);
 }
 
-int distancia_euclidiana(Ponto p1, Ponto p2)
+double distancia_euclidiana(Ponto p1, Ponto p2)
 {
-    return ceil(sqrt(abs(pow(p1.x - p2.x, 2.0) + pow(p1.y - p2.y, 2.0))));
+    
+    double diferenca_1 = p1.x - p2.x;
+    double quadrado_1 = pow(diferenca_1, 2.0);
+
+    double diferenca_2 = p1.y - p2.y;
+    double quadrado_2 = pow(diferenca_2, 2.0);
+    double distancia = sqrt( quadrado_1 + quadrado_2);
+
+    return distancia;
 }
 
 int calcula_distancia_e_insere_arestas(p_grafo g, Ponto *pontos)
@@ -62,49 +71,52 @@ int calcula_distancia_e_insere_arestas(p_grafo g, Ponto *pontos)
         {
             if (i != j)
             {
-                int x = distancia_euclidiana(pontos[i], pontos[j]);
+                double x = distancia_euclidiana(pontos[i], pontos[j]);
                 g->adj[i][j] = x;
-                if (x > maior_distancia){
-                    maior_distancia = x;
+                if (x > maior_distancia)
+                {
+                    maior_distancia = ceil(x);
                 }
             }
-            else {
-                g->adj[i][j] = 0;
+            else
+            {
+                g->adj[i][j] = -1;
             }
         }
     }
     return maior_distancia;
 }
 
-int eh_lugia(const int *categorias, int pos){
-
+int eh_lugia(const int *categorias, int pos)
+{
     return categorias[pos];
-
 }
 
 int busca_recur(p_grafo g, int *visitados, int vertice, int tam, const int *categorias)
 {
-  int w; 
-  //se v = fim, econtramos o caminho
+    int w;
+    //se v = fim, econtramos o caminho
     if (eh_lugia(categorias, vertice))
-  {
-      return 1;
-  }
-    
-  //caso contario, marca que visitou
-  visitados[vertice] = 1;
+    {
+        return 1;
+    }
 
-  //percorre vizinhos 
-  for(w = 0; w < g->n; w++){
-      //se a distancia for menor que ou igual ao tamanho atual e o vizinho ainda não foi visitado
-      if(g->adj[vertice][w] <= tam && g->adj[vertice][w] != 0 && !visitados[w]){
-          if(busca_recur(g, visitados, w, tam, categorias)){
-              return 1; 
-          }
-          
-      }
-  }
-    return 0; 
+    //caso contario, marca que visitou
+    visitados[vertice] = 1;
+
+    //percorre vizinhos
+    for (w = 0; w < g->n; w++)
+    {
+        //se a distancia for menor que ou igual ao tamanho atual e o vizinho ainda não foi visitado
+        if (g->adj[vertice][w] <= tam && g->adj[vertice][w] != -1 && !visitados[w])
+        {
+            if (busca_recur(g, visitados, w, tam, categorias))
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 
 /** Chama busca em profundidade,
@@ -114,8 +126,7 @@ int busca_recur(p_grafo g, int *visitados, int vertice, int tam, const int *cate
  * */
 int existe_caminho(p_grafo g, int ini, int tam, const int *categorias)
 {
-    int *visitado = malloc(g->n * sizeof(int));
-    int encontrou, i;
+    int encontrou, i, *visitado = malloc(g->n * sizeof(int));
     for (i = 0; i < g->n; i++)
     {
         visitado[i] = 0;
@@ -136,28 +147,30 @@ int busca_binaria(p_grafo g, int ini, int min, int max, const int *categorias)
 
     tam_atual = ceil((min + max) / 2);
     retorno = existe_caminho(g, ini, tam_atual, categorias);
-    
-    //se a diferença entre max e min é 1, é pq não existe menor ou igual ao mínimo, mas ao máx sim 
-    if(max - min == 1){
+
+    //se a diferença entre max e min é 1, é pq não existe menor ou igual ao mínimo, mas ao máx sim
+    if (max - min == 1)
+    {
         return max;
     }
 
     //se passou, pode existir existe caminho menor
-    if(retorno){
+    if (retorno)
+    {
         return busca_binaria(g, ini, min, tam_atual, categorias);
     }
-    //se não passou, existe caminho maior 
-    else{
+    //se não passou, existe caminho maior
+    else
+    {
         return busca_binaria(g, ini, tam_atual, max, categorias);
-    }   
+    }
 }
-
 
 int main()
 {
 
     int maior_distancia, retorno;
-    int i = 0, inicio = -1, menor_distancia = 0;
+    int i = 1, inicio = -1, menor_distancia = 0;
     double componente_x;
 
     //armazena todos os pontos lidos antes de inserir na matriz do grafo
@@ -195,10 +208,10 @@ int main()
         }
 
         retorno = scanf("%lf", &componente_x);
-        if (retorno != EOF){
+        if (retorno != EOF)
+        {
             i++;
         }
-        
 
     } while (retorno != EOF);
 
