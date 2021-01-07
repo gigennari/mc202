1892a86f80e9d9705e11cf2edf6d295b285fb64e
Giovanna_Gennari <g198010@dac.unicamp.br>
2021-01-07 09:52:29 -0300

    atualiza leitura de linha

--- tarefa12/planilha.c
+++ tarefa12/planilha.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define letra_A 65
 
 typedef struct Token
 {
     int valor;
-    char expressao[25];
+    char expressao[56];
 
 } Token;
 
@@ -29,7 +30,7 @@ p_planilha aloca_planilha(int m, int n)
 
     for (int i = 0; i < m; i++)
     {
-        p->planilha[i] = malloc(n * sizeof(Token));
+        p->planilha[i] = calloc(n, sizeof(Token));
     }
 
     p->visitados = malloc(m * sizeof(int *));
@@ -55,42 +56,44 @@ p_planilha zera_visitados(p_planilha p)
 
 void le_linha(p_planilha p, char *linha, int num_linha)
 {
-    char auxiliar[25];
+    char auxiliar[56];
     char caractere;
     int i = 0, coluna = 0, pos = 0;
 
     for (caractere = linha[i]; i < strlen(linha); i++)
     {
-        //se for diferente de espaço
-        if (caractere != " ")
+        //se for vírgula
+        if (caractere == ",")
         {
+            auxiliar[pos] = "\0";
 
-            //pode estar no fim, com uma vírgula, então devemos inserir na planilha
-            if (caractere == ",")
-                auxiliar[pos] = "\0";
             //se começar com "=" é expressão
             if (auxiliar[0] == "=")
             {
-                strcpy(p->planilha[num_linha][coluna]->expressao, auxiliar);
+                auxiliar[0] = "\0"; //elimina o =
+                strcpy(p->planilha[num_linha][coluna].expressao, auxiliar);
             }
+
             //caso contrário, é número
             else
             {
                 int valor = atoi(auxiliar);
-                p->planilha[num_linha][coluna]->valor = valor;
+                p->planilha[num_linha][coluna].valor = valor;
             }
             coluna++;
             pos = 0;
         }
-        //caso contrário, copiar caractere
+        //caso contrário, copiar caractere se for diferente de espaço
         else
         {
-            auxiliar[pos] = caractere;
-            pos++;
+            if (caractere != " ")
+            {
+                auxiliar[pos] = caractere;
+                pos++;
+            }
         }
     }
 }
-
 
 p_planilha le_arquivo(p_planilha p, char *nome_arquivo)
 {
@@ -111,19 +114,35 @@ p_planilha le_arquivo(p_planilha p, char *nome_arquivo)
     return p;
 }
 
-void leitura_e_calculo(p_planilha p, char *celula){
-
+int resolve_expressao(p_planilha p, char *expressao)
+{
 }
 
-void atualizacao(p_planilha p, char *celula, int novo_valor){
-    
+void leitura_e_calculo(p_planilha p, int linha, int coluna)
+{
+
+    //se for um número, a expressao é vazia
+    if (strlen(p->planilha[linha][coluna - letra_A].expressao) == 0)
+    {
+        printf("%c%d: %d", coluna, linha, p->planilha[linha][coluna].valor);
+    }
+
+    //se for uma expressão
+    else
+    {
+        int resultado = resolve_expressao(p, p->planilha[linha][coluna - letra_A].expressao);
+    }
+}
+
+void atualizacao(p_planilha p, int linha, int coluna, int novo_valor)
+{
 }
 
 int main()
 {
 
     //lê nome do arquivo e dimensões da planilha
-    char nome_arquivo[25];
+    char nome_arquivo[56];
     int m, n;
     scanf("%s %d %d", nome_arquivo, m, n);
 
@@ -134,29 +153,49 @@ int main()
     p = le_arquivo(p, nome_arquivo);
 
     //lê e realiza operações
-    int retorno;
+    int retorno = 1;
     char operacao;
-    
+
     scanf("%c", operacao);
-    do{
+    do
+    {
+
+        //lê célula
+        char celula[4];
+        int linha, coluna;
+        scanf("%s", celula);
+
+        //coluna a partir da letra (subtrair A na hora de procurar na matriz)
+        coluna = celula[0];
+
+        //calcula linha copiando o restante da str e usando atoi
+        int tam = strlen(celula);
+        char aux[4];
+        for (int i = 1; i < tam; i++)
+        {
+            aux[i - 1] = celula[i];
+        }
+
+        linha = atoi(aux);
+
         //operacao de leitura e calculo do valor atual
-        if(operacao == "G"){
-            char celula[10];
-            scanf("%c", celula);
-            leitura_e_calculo(p, celula);
+        if (operacao == "G")
+        {
+            leitura_e_calculo(p, linha, coluna);
         }
         //operacao de atualizar célula e calcular novo valor
-        else{
-            char celula[10];
+        else
+        {
             int novo_valor;
-            scanf("%s %d", celula, &novo_valor);
-            atualizacao(p, celula, novo_valor);
+            scanf("%d", &novo_valor);
+
+            atualizacao(p, linha, coluna, novo_valor);
         }
 
-        //lê próxima operação se existir 
+        //lê próxima operação se existir
         retorno = scanf("%c", operacao);
 
-    }while(retorno != EOF);
+    } while (retorno != EOF);
 
     return 0;
 }
