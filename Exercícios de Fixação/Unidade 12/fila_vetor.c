d75c9814030ab7e0786a18fb88c753458c858b09
Giovanna_Gennari <g198010@dac.unicamp.br>
2021-01-09 19:46:40 -0300

    inicializa/eh_vazia/eh_cheia fila com vetor

--- Exercícios de Fixação/Unidade 12/fila_vetor.c
+++ Exercícios de Fixação/Unidade 12/fila_vetor.c
@@ -4,40 +4,52 @@
 typedef struct Fila
 {
     int *v;
-    int ini, fim, N, tamanho;
+    int ini, fim, tam_max, tamanho;
 } Fila;
 
 typedef struct Fila *p_fila;
 
-p_fila criar_fila(int N)
+p_fila criar_fila(int tam_max)
 {
     p_fila f;
     f = malloc(sizeof(Fila));
-    f->v = malloc(N * sizeof(int));
+    f->v = malloc(tam_max * sizeof(int));
     f->ini = 0;
     f->fim = 0;
-    f->N = N;
+    f->tam_max = tam_max;
     f->tamanho = 0;
+
+    return f;
 }
 
 void enfileira(p_fila f, int x)
 {
     f->v[f->fim] = x;
-    f->fim = (f->fim + 1) % f->N;
+    f->fim = (f->fim + 1) % f->tam_max;
     f->tamanho++;
 }
 
 int desenfileira(p_fila f)
 {
     int x = f->v[f->ini];
-    f->ini = (f->ini + 1) % f->N;
+    f->ini = (f->ini + 1) % f->tam_max;
     f->tamanho--;
     return x;
 }
 
 int fila_vazia(p_fila f)
 {
-    return f->tamanho == 0;
+    if(f->tamanho == 0){
+        return 1;
+    }
+    return 0;
+}
+
+int fila_cheia(p_fila f){
+    if(f->tamanho == f->tam_max){
+        return 1;
+    }
+    return 0;
 }
 
 int main()
