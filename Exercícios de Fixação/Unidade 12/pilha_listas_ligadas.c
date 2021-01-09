e34b1ebea009f66f406ac48f3bf4e645f15fbdb2
Giovanna_Gennari <g198010@dac.unicamp.br>
2021-01-09 19:40:35 -0300

    inicializa/eh_vazia  pilha com lista ligada

--- Exercícios de Fixação/Unidade 12/pilha_listas_ligadas.c
+++ Exercícios de Fixação/Unidade 12/pilha_listas_ligadas.c
@@ -12,16 +12,23 @@ typedef struct No *p_no;
 typedef struct pilha
 {
     p_no topo;   // prox posição livre
+    int tam;
 }Pilha;
 
 typedef Pilha *p_pilha;
 
+p_pilha inicializa(){
+    p_pilha p = malloc(sizeof(Pilha));
+    p->topo = NULL;
+    p->tam = 0;
+}
 
 void empilhar(p_pilha pilha, int x){
     p_no novo = malloc(sizeof(No));
     novo->dado = x;
     novo->prox = pilha->topo;
     pilha->topo = novo;
+    pilha->tam += 1;
 }
 
 int desempilhar(p_pilha pilha){
@@ -29,5 +36,13 @@ int desempilhar(p_pilha pilha){
     int x = topo->dado;
     pilha->topo = pilha->topo->prox;
     free(topo);
+    pilha->tam -= 1;
     return x;
+}
+
+int eh_vazia(p_pilha pilha){
+    if(pilha->tam == 0){
+        return 1;
+    }
+    return 0;
 }
